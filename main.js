46f44673ff078955f3bf601c748735ac86bdc93e
Ulrika <u.sniedze@gmail.com>
2018-10-10 19:12:06 +0200

    Adds different colored customers and adds a legend

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 function init() {
-  createCustomers();
   setInterval(update, 5000);
   // update();
 }
@@ -9,8 +8,8 @@ init();
 function update() {
   let data = JSON.parse(FooBar.getData());
   console.log(data);
-  applyColor(data.queue.length);
-  // showCustomer(data.queue.length);
+
+  showCustomer(data.queue.length);
   // handleBarTenders(data.bartenders);
   // showTaps(data.taps)
 }
@@ -23,50 +22,22 @@ function showTaps(taps) {
   console.log(taps);
 }
 
-function createCustomers() {
-  const container = document.querySelector("section:nth-child(2) article");
-  for (let i = 0; i < 30; i++) {
-    let homer = document.createElement("img");
-    homer.src = "images/homer1.png";
-    // let homer = document.createElement("object");
-    // homer.type = "image/svg+xml";
-    // homer.data = "images/homer.svg";
-    container.appendChild(homer);
-  }
-}
-function applyColor(number) {
-  let homer = document.querySelectorAll("section:nth-child(2) article img");
-  if (number <= 5) {
-    console.log("too little");
-    for (let i = 0; i < number; i++) {
-      homer[i].src = "images/homer-red.png";
-    }
-    if (number >= 6 && number <= 9) {
-      console.log("too little");
-      for (let i = 0; i < number; i++) {
-        homer[i].src = "images/homer.png";
-      }
-    }
-    if (number > 9) {
-      homer[i].src = "images/homer-yellow.png";
-    }
-  }
-  showCustomer(number);
-}
-
 function showCustomer(number) {
   document.querySelector("#queue span").textContent = " " + number;
-  let allCustomers = document.querySelectorAll(
-    "section:nth-child(2) article img"
-  );
+  const container = document.querySelector("#container");
+  container.innerHTML = "";
+
   for (let i = 0; i < number; i++) {
-    allCustomers[i].style.opacity = "1";
-    allCustomers[i].style.transitionDelay = `${1 / 3}s`;
-  }
-  setTimeout(applyOpacity, 10000);
-  function applyOpacity() {
-    allCustomers.forEach(e => (e.style.opacity = "0"));
-    ///fetch again
-    update();
+    let homer = document.createElement("img");
+    if (number <= 5) {
+      homer.src = "images/homer-red.png";
+    }
+    if (number > 5 && number < 9) {
+      homer.src = "images/homer1.png";
+    }
+    if (number >= 9) {
+      homer.src = "images/homer-yellow.png";
+    }
+    container.appendChild(homer);
   }
 }
