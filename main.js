a51c9eda3a1cf7b2c644bdc43e712f2271b7df0b
Ulrika <u.sniedze@gmail.com>
2018-10-10 16:03:45 +0200

    Switched svg to img

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function init() {
+  createCustomers();
   setInterval(update, 5000);
   // update();
 }
@@ -8,7 +9,8 @@ init();
 function update() {
   let data = JSON.parse(FooBar.getData());
   console.log(data);
-  customersInQueue(data.queue.length, data.queue);
+  applyColor(data.queue.length);
+  // showCustomer(data.queue.length);
   // handleBarTenders(data.bartenders);
   // showTaps(data.taps)
 }
@@ -21,22 +23,50 @@ function showTaps(taps) {
   console.log(taps);
 }
 
-function customersInQueue(customers, data) {
-  console.log(customers);
-  console.log(data);
+function createCustomers() {
   const container = document.querySelector("section:nth-child(2) article");
-
-  container.innerHTML = "";
-  for (let i = 0; i < customers; i++) {
-    let homer = document.createElement("object");
-    homer.type = "image/svg+xml";
-    homer.data = "images/homer.svg";
-    homer.style.width = "50px";
-    let id = data[i].id;
-    homer.classList.add("homer" + id);
+  for (let i = 0; i < 30; i++) {
+    let homer = document.createElement("img");
+    homer.src = "images/homer1.png";
+    // let homer = document.createElement("object");
+    // homer.type = "image/svg+xml";
+    // homer.data = "images/homer.svg";
     container.appendChild(homer);
   }
-  let count = document.createElement("p");
-  count.textContent = customers;
-  container.appendChild(count);
+}
+function applyColor(number) {
+  let homer = document.querySelectorAll("section:nth-child(2) article img");
+  if (number <= 5) {
+    console.log("too little");
+    for (let i = 0; i < number; i++) {
+      homer[i].src = "images/homer-red.png";
+    }
+    if (number >= 6 && number <= 9) {
+      console.log("too little");
+      for (let i = 0; i < number; i++) {
+        homer[i].src = "images/homer.png";
+      }
+    }
+    if (number > 9) {
+      homer[i].src = "images/homer-yellow.png";
+    }
+  }
+  showCustomer(number);
+}
+
+function showCustomer(number) {
+  document.querySelector("#queue span").textContent = " " + number;
+  let allCustomers = document.querySelectorAll(
+    "section:nth-child(2) article img"
+  );
+  for (let i = 0; i < number; i++) {
+    allCustomers[i].style.opacity = "1";
+    allCustomers[i].style.transitionDelay = `${1 / 3}s`;
+  }
+  setTimeout(applyOpacity, 10000);
+  function applyOpacity() {
+    allCustomers.forEach(e => (e.style.opacity = "0"));
+    ///fetch again
+    update();
+  }
 }
